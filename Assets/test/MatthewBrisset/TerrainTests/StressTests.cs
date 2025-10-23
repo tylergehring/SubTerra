dd9db8b4de2c594b9cb8237bfd11bafdc55d0973
matthewbrisset <matthewbrisset@gmail.com>
2025-10-23 08:56:19 -0700

    Update StressTests.cs

--- Assets/test/MatthewBrisset/TerrainTests/StressTests.cs
+++ Assets/test/MatthewBrisset/TerrainTests/StressTests.cs
@@ -12,21 +12,22 @@ public class StressTests
     [Test]
     public void StressTest_LargeMapGeneration()
     {
+        Debug.Log("Running test for large map generation");
         // Generate noise
-        var noise = new GameObject().AddComponent<NoiseHandler>();
+        var noise = new GameObject("Noise Handler").AddComponent<NoiseHandler>();
         NoiseLayer n_layer = new NoiseLayer();
         n_layer.frequency = 10;
         n_layer.intensity = 10;
         noise.noise.Add(n_layer);
+        Debug.Log("Created noise object");
 
         // Generate chunk
-        var generator = new GameObject();
-        generator.AddComponent<StaticChunk>();
-        StaticChunk chunk = generator.GetComponent<StaticChunk>();
+        StaticChunk chunk = new GameObject("Test Chunk").AddComponent<StaticChunk>();
         chunk.noiseHandler = noise;
         Assert.IsNotNull(chunk.noiseHandler);
 
         int size = 500; // large test
+        Debug.Log("Created terrain with size: " + size);
         Stopwatch sw = Stopwatch.StartNew();
         var terrain = chunk.chunkSize = size;
 
