a44eae7a6da992b9a48a26c4a2a4f085c7a49436
BobTL3 <230417663+BobTL3@users.noreply.github.com>
2025-11-04 10:40:12 -0800

    Pickaxe Can Be used !!!
    Press E to pick up
    Q to drop and U to use !

--- Assets/src/JackKroll/Scripts/PickaxeTool.cs
+++ Assets/src/JackKroll/Scripts/PickaxeTool.cs
@@ -5,12 +5,19 @@ public class PickaxeTool : MonoBehaviour
     public float breakRadius = 10f; // Radius to destroy chunks
 
     private TerrainHandler _terrain;
-  
-   
+
+    public Transform player;
+    
+    
 
     private void Start()
     {
         _terrain = FindFirstObjectByType<TerrainHandler>();
+
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
    
 
@@ -20,8 +27,23 @@ void Update()
         //cnages position 
         if (gameObject.activeSelf) // checks if the prefab (this GameObject) is active
         {
-            transform.position = new Vector3(115f, 88f, 0f);
+            transform.position = player.position + player.forward * 1f;
+
+
+
+            // Keep Z position fixed at 1
+            var pos = transform.position;
+            pos.y = player.position.x + -0.5f;
+            pos.y = player.position.y + 0.4f;
+            pos.z = -1f;
+            transform.position = pos;
+
+
         }
+              
+            
+
+        
 
         if (Input.GetKeyDown(KeyCode.U))
         {
