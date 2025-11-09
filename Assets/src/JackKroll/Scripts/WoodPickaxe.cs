ff53e6da695cc2343ff23a5f5b0fffd8d9b8ad72
BobTL3 <230417663+BobTL3@users.noreply.github.com>
2025-11-09 15:49:01 -0800

    NewPrefabs

---
+++ Assets/src/JackKroll/Scripts/WoodPickaxe.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class WoodPickaxe : UtilityTool
+
+{
+    private AudioSource audioSource;
+private float breakRadius = 1.6f; // Radius to destroy chunks
+
+private TerrainHandler _terrain;
+
+private Transform player;
+private float rotationSpeed = 40f; // degrees per second
+
+
+private void Start()
+{
+    audioSource = GetComponent<AudioSource>();
+
+    _terrain = FindFirstObjectByType<TerrainHandler>();
+
+    if (player == null)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+}
+
+
+void Update()
+{
+
+    //cnages position 
+    if (gameObject.activeSelf) // checks if the prefab (this GameObject) is active
+    {
+        transform.position = player.position + player.forward * 1f;
+
+
+
+        // Keep Z position fixed at 1
+        var pos = transform.position;
+        pos.y = player.position.x + -0.5f;
+        pos.y = player.position.y + 0.1f;
+        pos.z = -1f;
+        transform.position = pos;
+
+        if ((Input.GetMouseButton(0) && !Input.GetKeyDown(KeyCode.F)) || ((!Input.GetMouseButton(0) && Input.GetKeyDown(KeyCode.F)))) // Left mouse button held down
+        {
+            // Rotate around the Y axis (you can change to X/Z as needed)
+            transform.Rotate(0f, rotationSpeed * Time.deltaTime, 90f);
+
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+
+
+
+
+    if ((Input.GetMouseButton(0) && !Input.GetKeyDown(KeyCode.F)) || ((!Input.GetMouseButton(0) && Input.GetKeyDown(KeyCode.F)))) // Left mouse button held down
+    {
+        DestroyChunksAtPosition();
+    }
+
+}
+
+// added by Connor
+public void DoThing()
+{
+    DestroyChunksAtPosition();
+}
+
+private void DestroyChunksAtPosition()
+{
+    if (_terrain != null)
+    {
+        _terrain.DestroyInRadius(transform.position, breakRadius);
+    }
+}
+}
