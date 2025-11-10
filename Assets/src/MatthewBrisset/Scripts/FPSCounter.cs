e6c8b67a5c893575dd409d1dab5b764be246ff8b
matthewbrisset <matthewbrisset@gmail.com>
2025-11-09 18:25:24 -0800

    Small changes

---
+++ Assets/src/MatthewBrisset/Scripts/FPSCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    private TextMeshProUGUI displayText;
+    private float timer;
+    private float fps;
+
+    private void Start()
+    {
+        displayText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Update()
+    {
+        fps = Mathf.Lerp(fps, 1.0f / Time.deltaTime, Time.deltaTime);
+
+        displayText.text = Mathf.Ceil(fps).ToString();
+
+    }
+}
