988bafb8cbf6cc6688bf0dfe9b3b34fecdcafb8f
itsLuke6283 <lukezawis@gmail.com>
2025-12-10 19:38:05 -0800

    Revert "ui"
    This reverts commit 473ee40a9fcbdfd25e99362b8d1d2b1f92868b5f.

--- Assets/src/ConnorWolfe/Scripts/StaminaWheelScript.cs
+++ Assets/src/ConnorWolfe/Scripts/StaminaWheelScript.cs
@@ -1,47 +1,55 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class StaminaWheelScript : MonoBehaviour
 {
-    /* This script handles the stamina wheel UI elements
-     *
-     */
-    public float maxStamina = 100f;
+    /* This script handles the stamina wheel UI elements
+     * 
+     */
+
+
+    public float maxStamina = 100f;
+
     [SerializeField] private float _regen = 10f;
     [SerializeField] private float _hideTime = 3f;
     [SerializeField] private Image _greenWheel;
     [SerializeField] private Image _redWheel;
+
     private float _currStamina;
     private float _showTimer = 0f;
     private float _regenTime = 1f;
     private float _regenTimer = 0f;
     private float _redBuffer = 0f;
+
     private bool _staminaExhausted = false;
     private bool _showWheel = true;
+
     private void Start()
     {
         _currStamina = maxStamina;
     }
+
     private void Update()
     {
-        /*
-                if (_currStamina == maxStamina && _showTimer < _hideTime)
-                {
-                    _showTimer += Time.deltaTime;
-                }
-        */
+/*
+        if (_currStamina == maxStamina && _showTimer < _hideTime)
+        {
+            _showTimer += Time.deltaTime;
+        }
+*/
         if (!_staminaExhausted)
         {
-            /* if (_currStamina > 0)
-                        {
-                            _currStamina -= 30 * Time.deltaTime;
-                        }
-                        else
-                        {
-                            _greenWheel.enabled = false;
-                            _staminaExhausted = true;
-                        }
-            */
-
+            /*            if (_currStamina > 0)
+                        {
+                            _currStamina -= 30 * Time.deltaTime;
+                        }
+                        else
+                        {
+                            _greenWheel.enabled = false;
+                            _staminaExhausted = true;
+                        }
+            */
+            
             if (_regenTimer >= _regenTime)
             {
                 _currStamina += _regen * Time.deltaTime;
@@ -53,7 +61,7 @@ public class StaminaWheelScript : MonoBehaviour
             }
             _redWheel.fillAmount = (_currStamina / maxStamina + _redBuffer);
             if (_redBuffer >= 0)
-                _redBuffer -= _redBuffer * Time.deltaTime * 2.0f;
+            _redBuffer -= _redBuffer * Time.deltaTime * 2.0f;
         }
         else
         {
@@ -66,14 +74,19 @@ public class StaminaWheelScript : MonoBehaviour
                 _greenWheel.enabled = true;
                 _staminaExhausted = false;
             }
-            _redWheel.fillAmount = _currStamina / maxStamina;
+
+                _redWheel.fillAmount = _currStamina / maxStamina;
         }
+
         if (_regenTimer <= _regenTime)
             _regenTimer += Time.deltaTime;
+
         _greenWheel.fillAmount = _currStamina / maxStamina;
     }
-    // public functions //
-    public void ChangeStamina(float amount)
+
+
+    // public functions //
+    public void ChangeStamina(float amount)
     {
         _regenTimer = 0f;
         _redBuffer = 0.07f;
@@ -81,16 +94,16 @@ public class StaminaWheelScript : MonoBehaviour
         {
             _currStamina = maxStamina;
             return;
-        }
-        else if (_currStamina + amount <= 0)
-        {
+        } else if (_currStamina + amount <= 0) {
             _currStamina = 0;
             return;
         }
         _currStamina += amount;
     }
+
     public bool IsExhausted()
     {
         return _staminaExhausted;
     }
+
 }
